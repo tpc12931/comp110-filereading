d359d4145dce16c84d1834e2ff5ed7ac53085c9d
Sat Garcia <sat@sandiego.edu>
2020-02-25 13:40:48 -0800

    updated some comments and added some starter code for test_contains_word

--- comp110_lab05.py
+++ comp110_lab05.py
@@ -4,15 +4,15 @@ Module: comp110_lab05
 Exercises from lab 05, dealing with strings and file reading.
 """
 
-def max_windspeed(filename):
+def max_wind_speed(hurricane_filename):
     """
-    Finds the maximum windspeed for the hurricane.
+    Finds the maximum wind speed for the hurricane.
 
     Parameters:
-    1. filename (type: string) - Name of the file containing the data
+    1. hurricane_filename (type: string) - Name of the file containing the data
 
     Returns:
-    (type: int) The maximum windspeed of the hurricane.
+    (type: int) The maximum wind speed of the hurricane.
     """
 
     pass # replace this line with your implementation of this function
@@ -22,7 +22,7 @@ def contains_word(word, review):
     """
     Determines whether the review contains the given word. 
 
-    Note that this should work, regardless of case. For example "ok" is
+    Note that should ignore the "casing" of letters. For example "ok" is
     considered to be contained in the review "It was an OK movie."
 
     Parameters:
@@ -36,31 +36,43 @@ def contains_word(word, review):
 
     pass # replace this line with your implementation of this function
 
-def test_max_windspeed():
-    """ Function that tests the max_windspeed function. """
 
-    # To Do: Call your max_windspeed function on the irma.csv file.
-    # Then use an if statement to check if the result was the expect result.
-    # If it is, print out the message "max_windspeed('irma.csv') PASSED".
-    # Otherwise, print out the message "max_windspeed('irma.csv') FAILED".
+def test_max_wind_speed():
+    """ Function that tests the max_wind_speed function. """
 
-    # To Do: Repeat the previous "to do" but testing out florence.csv and
-    # dorian.csv.
+    print("Starting test of max_wind_speed")
 
-    print("Done testing max_windspeed")
+    # To Do: Call your max_wind_speed function on the irma.csv file, using an if
+    # statement to indicate whether the result was correct or not.
+    # Then repeat the process for the florence.csv and dorian.csv files to check
+    # whether your function works for those files.
+
+    print("Done testing max_wind_speed")
+
 
 def test_contains_word():
     """ Function that tests the contains_word function. """
 
-    # To Do: Call your contains_word function for the word/review combination
-    # given in the lab write-up. Again, you should check the return value and
-    # print a message to indicate whether each test case passed or failed.
+    print("Starting test of contains word")
+
+    if contains_word('ok', 'ok') != True:
+        print("Test failed: contains_word('ok', 'ok')")
+    elif contains_word('ok', 'OK') != True:
+        print("Test failed: contains_word('ok', 'OK')")
+    elif contains_word('ok', 'bad') != False:
+        print("Test failed: contains_word('ok', 'bad')")
+    elif contains_word('ok', 'not ok') != True:
+        print("Test failed: contains_word('ok', 'ok')")
+    # To Do: update the chained conditional to test all of your new test cases.
+    else:
+        print("All contains_word test cases passed!")
+
 
     print("Done testing contains word")
 
 
 def main():
-    test_max_windspeed()
+    test_max_wind_speed()
     test_contains_word()
 
 # Do not modify anything after this point.
